69cc7357fb7fb583cbbcf0a964ed831ffb18e463
JinChristine <jin.christine_0111@hotmail.com>
2025-09-17 15:52:02 +0200

    création: navbar + routing version 1

--- exo2-christine-steven/src/app/app.routes.ts
+++ exo2-christine-steven/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Accueil } from './accueil/accueil';
+import { Liste } from './liste/liste';
+import { Gestion } from './gestion/gestion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Accueil},
+    {path: 'liste', component: Liste},
+    {path: 'gestion', component: Gestion}
+];
