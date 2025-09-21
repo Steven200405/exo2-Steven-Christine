8f4423754a2886c91c6d71fa872ffe89b5d2bbba
Steven200405 <huangsteven2004@gmail.com>
2025-09-21 20:41:11 +0200

    Ajout des films

--- exo2-christine-steven/src/app/liste/liste.ts
+++ exo2-christine-steven/src/app/liste/liste.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './liste.scss'
 })
 export class Liste {
+  public image: string = "";
 
+  public afficherImage(film : string){
+    if(film === "avatar"){
+      this.image = "avatar.jpg";
+    }
+    else if(film === "lilo_stitch"){
+      this.image = "lilo_stitch.jpg";
+    }
+    else if(film === "conjuring"){
+      this.image = "conjuring.jpg";
+    }
+    else if(film === "fast_furious"){
+      this.image = "fast_furious.jpg";
+    }
+    else if(film === "indiana_jones"){
+      this.image = "indiana_jones.jpg";
+    }
+  }
 }
