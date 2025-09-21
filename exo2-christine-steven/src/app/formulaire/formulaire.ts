5f291401955137161bed0d7e21ddfd7cd060248f
JinChristine <jin.christine_0111@hotmail.com>
2025-09-21 23:51:08 +0200

    contact v2

--- exo2-christine-steven/src/app/formulaire/formulaire.ts
+++ exo2-christine-steven/src/app/formulaire/formulaire.ts
@@ -1,21 +1,22 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './formulaire.html',
   styleUrl: './formulaire.scss'
 })
 export class Formulaire {
-  public profileForm = new FormGroup(
+public profileForm = new FormGroup(
     {
-      firstname: new FormControl(''),
-      lastname: new FormControl(''),
+      firstname: new FormControl('',Validators.required),
+      lastname: new FormControl('',Validators.required),
       age: new FormControl(''),
       checkbox: new FormControl(false),
-      email: new FormControl('', Validators.email),
-      comment: new FormControl('')
+      email: new FormControl('', [Validators.required, Validators.email]),
+      comment: new FormControl('', Validators.required)
     }
   );
 
@@ -24,12 +25,20 @@ export class Formulaire {
   }
 
   public initEmail(): void {
-      this.profileForm.patchValue({
-        email: "",
-      });
+    if(this.profileForm.get('checkbox')?.value == false ){
+            this.profileForm.get('email')?.removeValidators(Validators.required);
+            this.profileForm.get('email')?.removeValidators(Validators.email);
+            this.profileForm.patchValue({
+              email: "",
+            });
+    }
+    else{
+      this.profileForm.get('email')?.setValidators([Validators.required, Validators.email]);
+    }
+    this.profileForm.updateValueAndValidity();
   }
 
   public submitForm(): void {
-    console.log(this.profileForm.get('firstname') + 'has submitted');
+    console.log(this.profileForm.get('firstname')?.value + 'has submitted');
   }
 }
